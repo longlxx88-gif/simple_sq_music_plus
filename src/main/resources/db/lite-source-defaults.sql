3fd5a77dbc72b7e2bbb82c134679690bbb65cb85
SQMUSIC_LITE <sqmusic-lite@users.noreply.github.com>
2026-09-09 09:17:28 +0700

    fix: escape music template during resource filtering

--- src/main/resources/db/lite-source-defaults.sql
+++ src/main/resources/db/lite-source-defaults.sql
@@ -16,5 +16,5 @@ WHERE NOT EXISTS (SELECT 1 FROM sq_config WHERE config_key = 'plug.tidal.token')
 -- files written beside the audio file. The root itself is supplied by
 -- system.download.path (mounted as /music in the container).
 INSERT INTO sq_config (config_name, config_value, config_key, config_type, config_show, config_null_check, config_disabled)
-SELECT '下载歌曲文件名模板', '${artists}/${album}/${musicName} - ${artists}', 'system.download.file.template', 'input', 1, 1, 0
+SELECT '下载歌曲文件名模板', '\${artists}/\${album}/\${musicName} - \${artists}', 'system.download.file.template', 'input', 1, 1, 0
 WHERE NOT EXISTS (SELECT 1 FROM sq_config WHERE config_key = 'system.download.file.template');
